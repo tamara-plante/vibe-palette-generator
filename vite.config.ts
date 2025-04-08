9a4af00c7359547f8815e537c7902a46cbcccae0
tamara-plante <0631064@johnabbottcollege.net>
2025-04-08 17:37:30 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig(({ mode }) => ({
     plugins: [react(), mode === 'development' && componentTagger()].filter(
         Boolean
     ),
-    base: '/vibe-palette-generator',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
